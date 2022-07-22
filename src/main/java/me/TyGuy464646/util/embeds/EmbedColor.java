a516b701b8b1bfb13106f3c46c9b5860b3a7d48d
TyGuy464646 <t.eldred4646@gmail.com>
2022-07-21 21:50:15 -0600

    Added default embed colors.

---
+++ src/main/java/me/TyGuy464646/util/embeds/EmbedColor.java
@@ -0,0 +1,14 @@
+package me.TyGuy464646.util.embeds;
+
+public enum EmbedColor {
+    DEFAULT(0x57a2ff),
+    ERROR(0xdd5f53),
+    SUCCESS(0x77b255),
+    WARNING(0xff8c03);
+
+    public final int color;
+
+    EmbedColor(int hexCode) {
+        this.color = hexCode;
+    }
+}
